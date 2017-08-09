8f867043670e2604637fa82595292478031cd7ac
tony <tony@tonydeMacBook-Pro.local>
2017-08-09 18:31:36 +0800

    modify audit_shell.py

--- audit_shell.py
+++ audit_shell.py
@@ -8,6 +8,5 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LuffyAudit.settings")
     import django
     django.setup() #手动注册django所有的APP
-
     obj = user_interactive.UserShell(sys.argv)
     obj.start()
