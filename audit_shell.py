a2bc0d56a7257ea89026261bbca1ad214101da18
yangxiang <18614085772@163.com>
2017-08-09 19:44:28 +0800

    audit_shell文件修改

--- audit_shell.py
+++ audit_shell.py
@@ -5,6 +5,7 @@ from audit.backend import user_interactive
 
 
 if __name__ == "__main__":
+    #外部py文件执行，需要加"DJANGO_SETTINGS_MODULE", "LuffyAudit.settings"字段
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LuffyAudit.settings")
     import django
     django.setup() #手动注册django所有的APP
