cc84c6a47ba73447162f3bd421cf4984c005a77c
孙 其 <sunq@dawangjiaowolaixunshandeMacBook-Pro.local>
2017-08-09 18:48:22 +0800

    alt+command+L

--- audit/backend/user_interactive.py
+++ audit/backend/user_interactive.py
@@ -5,7 +5,7 @@ from django.contrib.auth import authenticate
 class UserShell(object):
     """用户登录堡垒机后的shell"""
 
-    def __init__(self,sys_argv):
+    def __init__(self, sys_argv):
         self.sys_argv = sys_argv
         self.user = None
 
@@ -15,15 +15,15 @@ class UserShell(object):
         while count < 3:
             username = input("username:").strip()
             password = input("password:").strip()
-            user = authenticate(username=username,password=password)
-            #None 代表认证不成功
-            #user object ，认证对象 ,user.name
+            user = authenticate(username=username, password=password)
+            # None 代表认证不成功
+            # user object ，认证对象 ,user.name
             if not user:
                 count += 1
                 print("Invalid username or password!")
             else:
                 self.user = user
-                return  True
+                return True
         else:
             print("too many attempts.")
 
@@ -31,34 +31,32 @@ class UserShell(object):
         """启动交互程序"""
 
         if self.auth():
-            #print(self.user.account.host_user_binds.all()) #select_related()
+            # print(self.user.account.host_user_binds.all()) #select_related()
             while True:
                 host_groups = self.user.account.host_groups.all()
-                for index,group in enumerate(host_groups):
-                    print("%s.\t%s[%s]"%(index,group,group.host_user_binds.count()))
-                print("%s.\t未分组机器[%s]"%(len(host_groups),self.user.account.host_user_binds.count()))
+                for index, group in enumerate(host_groups):
+                    print("%s.\t%s[%s]" % (index, group, group.host_user_binds.count()))
+                print("%s.\t未分组机器[%s]" % (len(host_groups), self.user.account.host_user_binds.count()))
 
                 choice = input("select group>:").strip()
                 if choice.isdigit():
                     choice = int(choice)
                     host_bind_list = None
-                    if choice >=0 and choice < len(host_groups):
+                    if choice >= 0 and choice < len(host_groups):
                         selected_group = host_groups[choice]
                         host_bind_list = selected_group.host_user_binds.all()
-                    elif choice == len(host_groups): #选择的未分组机器
-                        #selected_group = self.user.account.host_user_binds.all()
+                    elif choice == len(host_groups):  # 选择的未分组机器
+                        # selected_group = self.user.account.host_user_binds.all()
                         host_bind_list = self.user.account.host_user_binds.all()
                     if host_bind_list:
                         while True:
-                            for index,host in enumerate(host_bind_list):
-                                print("%s.\t%s"%(index,host,))
+                            for index, host in enumerate(host_bind_list):
+                                print("%s.\t%s" % (index, host,))
                             choice2 = input("select host>:").strip()
                             if choice2.isdigit():
                                 choice2 = int(choice2)
-                                if choice2 >=0 and choice2 < len(host_bind_list):
+                                if choice2 >= 0 and choice2 < len(host_bind_list):
                                     selected_host = host_bind_list[choice2]
-                                    print("selected host",selected_host)
+                                    print("selected host", selected_host, '\t-----')
                             elif choice2 == 'b':
                                 break
-
-
