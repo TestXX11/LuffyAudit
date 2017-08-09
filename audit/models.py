3813c19dbf4a1015e8e7f54aea6a6571a8f06f54
DragonFire <longbaby1101@qq.com>
2017-08-09 17:51:48 +0800

    moudels

--- audit/models.py
+++ audit/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import  User
 # Create your models here.
-
-
 class IDC(models.Model):
     name = models.CharField(max_length=64,unique=True)
     def __str__(self):
