d038cdf51caffd8bbe8ace07dbb130ce437de11e
DragonFire <longbaby1101@qq.com>
2017-08-11 02:16:48 +0800

    ssh登录+进程监测

--- start_audit.py
+++ start_audit.py
@@ -1,9 +1,9 @@
 __author__ = 'Administrator'
 import sys,os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LuffyAudit.settings")
+import django
+from audit.backend import user_welcome
 if __name__ == "__main__":
-    import django
-    from audit.backend import user_welcome
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "LuffyAudit.settings")
     django.setup()  # 手动注册django所有的APP
     us = user_welcome.UserShell(sys.argv)
     us.welcome()
