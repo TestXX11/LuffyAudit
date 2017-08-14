48c4f4fdb3e31cac171a9ab244bce4963be9cf8b
jack <justgofor@qq.com>
2017-08-14 20:25:51 +0800

    use templates to develop web backend -v.0

--- jackupdownAudit/settings.py
+++ jackupdownAudit/settings.py
@@ -121,3 +121,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 SESSION_TRACKER_SCRIPT = os.path.join(BASE_DIR, 'audit/backend/session_tracker.sh')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'statics/'),
+
+)
+LOGIN_URL = '/login/'
--- jackupdownAudit/urls.py
+++ jackupdownAudit/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from audit import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', views.index),
+    url(r'^login/$', views.acc_login),
+    url(r'^logout/$', views.acc_logout),
+    url(r'^hostlist/$', views.host_list, name="host_list"),
+    url(r'^api/hostlist/$', views.get_host_list, name="get_host_list"),
 ]
