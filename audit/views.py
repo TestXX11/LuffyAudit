f7ad945bcec84de7962ce04b86ae81eabc1a2670
jack <justgofor@qq.com>
2017-08-15 17:30:40 +0800

    add shellinabox to execute command on the web

--- audit/views.py
+++ audit/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect, HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 import json
+from audit import models
+import random
+import string
+import datetime
 
 
 # Create your views here.
@@ -28,7 +32,6 @@ def acc_login(request):
 @login_required
 def acc_logout(request):
     logout(request)
-
     return redirect('/login/')
 
 
@@ -43,12 +46,34 @@ def get_host_list(request):
     if gid:
         if gid == '-1':  # 未分组
             host_list = request.user.account.host_user_binds.all()
-            print('-1111111')
-            print(host_list)
         else:
             group_obj = request.user.account.host_groups.get(id=gid)
             host_list = group_obj.host_user_binds.all()
-
-        data = json.dumps(list(host_list.values('id', 'host__hostname', 'host__ip_addr', 'host__idc__name', 'host__port',
-                                                'host_user__username')))
+        data = json.dumps(
+            list(host_list.values('id', 'host__hostname', 'host__ip_addr', 'host__idc__name', 'host__port',
+                                  'host_user__username')))
         return HttpResponse(data)
+
+
+@login_required
+def get_token(request):
+    """生成token并返回"""
+    bind_host_id = request.POST.get('bind_host_id')
+    time_obj = datetime.datetime.now() - datetime.timedelta(seconds=300)
+    exist_token_objs = models.Token.objects.filter(account_id=request.user.account.id,
+                                                   host_user_bind_id=bind_host_id,
+                                                   date__gt=time_obj)
+    if exist_token_objs:
+        token_data = {'token': exist_token_objs[0].val}
+    else:
+        token_val = ''.join(random.sample(string.ascii_lowercase + string.digits, 8))
+        token_obj = models.Token.objects.create(
+            host_user_bind_id=bind_host_id, account=request.user.account, val=token_val
+        )
+        token_data = {'token': token_val}
+    return HttpResponse(json.dumps(token_data))
+
+
+@login_required
+def multi_cmd(request):
+    return render(request, 'multi_cmd.html')
